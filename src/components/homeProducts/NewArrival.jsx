85aa91485f9562b2a9e1953478df83888d4e16c0
Aritra Karan <karanaritra19@gmail.com>
2025-12-29 01:08:48 +0530

    test grid issue

--- src/components/homeProducts/NewArrival.jsx
+++ src/components/homeProducts/NewArrival.jsx
@@ -71,7 +71,7 @@ const NewArrivals = () => {
         <div className="flex items-center justify-between">
           <Title className="text-2xl mb-3 font-bold">New Arrivals</Title>
         </div>
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {Array.from({ length: 4 }).map((_, index) => (
             <div
               key={index}
@@ -109,7 +109,7 @@ const NewArrivals = () => {
         </Slider>
       ) : (
         // Use simple grid when 3 or fewer products
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {products?.map((item) => (
             <ProductCard item={item} key={item?._id} />
           ))}
