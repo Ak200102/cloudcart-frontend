b6356c091812172edb1caa07ab390f521de182a1
Aritra Karan <karanaritra19@gmail.com>
2025-12-31 01:40:16 +0530

    issue fixed

--- src/components/homeProducts/SpecialOffer.jsx
+++ src/components/homeProducts/SpecialOffer.jsx
@@ -38,7 +38,7 @@ const SpecialOffers = () => {
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
         },
