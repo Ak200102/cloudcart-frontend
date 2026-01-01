80f6ae2f506abe7957f05799791cd30eb7ad636e
Aritra Karan <karanaritra19@gmail.com>
2026-01-02 00:08:04 +0530

    related product responsiveness fixed

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -422,7 +422,7 @@ const SingleProduct = () => {
               ))}
             </div>
           ) : relatedProducts.length > 0 ? (
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+            <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-8">
               {relatedProducts.map((product) => (
                 <div
                   key={product._id}
